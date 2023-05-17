2252e53fe7cc760832c8ace136c294c751880c43
mhmad-alaa <ma2001129@gmail.com>
2023-05-17 16:07:30 +0300

    fix naming variables

--- main.cpp
+++ main.cpp
@@ -7,7 +7,9 @@
 double r, theta;
 
 // Angular position displacement variables
-double m = 0, v = 0, e = 0, mr = 0, j = 0, s = 0, u = 0, n = 0;
+double mercury_angle = 0, venus_angle = 0, earth_angle = 0,
+       mars_angle = 0, jupiter_angle = 0, saturn_angle = 0,
+       uranus_angle = 0, neptune_angle = 0;
 
 // Planet position variables declaration and initialization
 double mercury_x = 400;
@@ -28,23 +30,23 @@ double neptune_x = 1800;
 double neptune_y = 0;
 
 // Sun halo radius variable
-double sun_r = 250;
+double sun_radius = 250;
 
 // Sun halo increment/decrement
-int sr = 1;
+int sun_halo_radius = 1;
 
 // Timer stop flag
 bool stop = false;
 
-// Random star positions
-double zx[200];
-double zy[200];
+// Random star positions stars[i][0] for x-axis & stars[i][1] for y-axis
+double stars[200][2];
 
 // Timer callback function
 void timer(int);
 
 // Planet position reset function
-void resetPlanetPositions() {
+void resetPlanetPositions()
+{
     mercury_x = 400;
     mercury_y = 0;
     venus_x = 600;
@@ -61,49 +63,59 @@ void resetPlanetPositions() {
     uranus_y = 0;
     neptune_x = 1800;
     neptune_y = 0;
-    n = 0;
-    u = 0;
-    s = 0;
-    j = 0;
-    mr = 0;
-    e = 0;
-    v = 0;
-    m = 0;
+    neptune_angle = 0;
+    uranus_angle = 0;
+    saturn_angle = 0;
+    jupiter_angle = 0;
+    mars_angle = 0;
+    earth_angle = 0;
+    venus_angle = 0;
+    mercury_angle = 0;
 }
 
 // Keyboard callback function
-void keyboard(unsigned char c, int x, int y) {
-    if (c == 27) exit(0); // Escape key
-    if (c == 's' || c == 'S') {
+void keyboard(unsigned char c, int x, int y)
+{
+    if (c == 27)
+        exit(0); // Escape key
+    if (c == 's' || c == 'S')
+    {
         stop = !stop;
-        if (!stop) {
-            timer(0);  // Start the timer immediately
+        if (!stop)
+        {
+            timer(0); // Start the timer immediately
         }
     };
-    if (c == 'r' || c == 'R') resetPlanetPositions();
+    if (c == 'r' || c == 'R')
+        resetPlanetPositions();
 }
 
 // Drawing functions
-void drawStars() {
+void drawStars()
+{
     // Draw 200 random stars
-    for (int z = 0; z <= 200; z++) {
+    for (int z = 0; z <= 200; z++)
+    {
         glPointSize(1.5);
         glBegin(GL_POINTS);
         glColor3ub(255, 255, 255);
         // Positions obtained from loop in main()
-        glVertex2f(zx[z], zy[z]);
+        glVertex2f(stars[z][0], stars[z][1]);
         glEnd();
     }
 }
 
-void drawOrbits() {
+void drawOrbits()
+{
     // Draw orbits in descending order
     r = 1800;
     glPointSize(1.0f);
     glBegin(GL_POINTS);
     glColor3ub(255, 255, 255);
-    for (int j = 0; j < 8; j++) {
-        for (int i = 0; i <= 360; i++) {
+    for (int j = 0; j < 8; j++)
+    {
+        for (int i = 0; i <= 360; i++)
+        {
             theta = i * 3.142 / 180;
             glVertex2f(r * cos(theta), r * sin(theta));
         }
@@ -113,22 +125,25 @@ void drawOrbits() {
     glEnd();
 }
 
-void drawCelestialBodies() {
+void drawCelestialBodies()
+{
 
     // Sun halo
     glBegin(GL_POLYGON);
     // Same as Sun color, but with 50% transparency
-    glColor4f(1, 0.8, 0.098,0.5);
-    for (int i = 0; i <= 360; i++) {
+    glColor4f(1, 0.8, 0.098, 0.5);
+    for (int i = 0; i <= 360; i++)
+    {
         theta = i * 3.142 / 180;
-        glVertex2f(sun_r * cos(theta), sun_r * sin(theta)); // `sun_r` (halo radius) is updated every timer call
+        glVertex2f(sun_radius * cos(theta), sun_radius * sin(theta)); // `sun_radius` (halo radius) is updated every timer call
     }
     glEnd();
 
     // Sun
     glBegin(GL_POLYGON);
     glColor3ub(254, 204, 25);
-    for (int i = 0; i <= 360; i++) {
+    for (int i = 0; i <= 360; i++)
+    {
         theta = i * 3.142 / 180;
         glVertex2f(250 * cos(theta), 250 * sin(theta)); // Sun radius = 250
     }
@@ -137,7 +152,8 @@ void drawCelestialBodies() {
     // Mercury
     glBegin(GL_POLYGON);
     glColor3ub(204, 126, 56);
-    for (int i = 0; i <= 360; i++) {
+    for (int i = 0; i <= 360; i++)
+    {
         theta = i * 3.142 / 180;
         glVertex2f(50 * cos(theta) + mercury_x, 50 * sin(theta) + mercury_y); // Mercury radius = 50
         // x and y positions are updated every timer call
@@ -147,7 +163,8 @@ void drawCelestialBodies() {
     // Venus
     glBegin(GL_POLYGON);
     glColor3ub(215, 122, 98);
-    for (int i = 0; i <= 360; i++) {
+    for (int i = 0; i <= 360; i++)
+    {
         theta = i * 3.142 / 180;
         glVertex2f(80 * cos(theta) + venus_x, 80 * sin(theta) + venus_y);
     }
@@ -156,7 +173,8 @@ void drawCelestialBodies() {
     // Earth
     glBegin(GL_POLYGON);
     glColor3ub(70, 248, 202);
-    for (int i = 0; i <= 360; i++) {
+    for (int i = 0; i <= 360; i++)
+    {
         theta = i * 3.142 / 180;
         glVertex2f(80 * cos(theta) + earth_x, 80 * sin(theta) + earth_y);
     }
@@ -168,7 +186,8 @@ void drawCelestialBodies() {
     // Green
     glColor3ub(125, 223, 63);
     // Draw the polygon's outside curve tracing the curvature of the Earth
-    for (int i = 200; i <= 260; i++) {
+    for (int i = 200; i <= 260; i++)
+    {
         theta = i * 3.142 / 180;
         glVertex2f(80 * cos(theta) + earth_x, 80 * sin(theta) + earth_y);
     }
@@ -180,7 +199,8 @@ void drawCelestialBodies() {
 
     glBegin(GL_POLYGON);
     glColor3ub(125, 223, 63);
-    for (int i = 280; i <= 350; i++) {
+    for (int i = 280; i <= 350; i++)
+    {
         theta = i * 3.142 / 180;
         glVertex2f(80 * cos(theta) + earth_x, 80 * sin(theta) + earth_y);
     }
@@ -191,7 +211,8 @@ void drawCelestialBodies() {
 
     glBegin(GL_POLYGON);
     glColor3ub(125, 223, 63);
-    for (int i = 70; i <= 130; i++) {
+    for (int i = 70; i <= 130; i++)
+    {
         theta = i * 3.142 / 180;
         glVertex2f(80 * cos(theta) + earth_x, 80 * sin(theta) + earth_y);
     }
@@ -204,7 +225,8 @@ void drawCelestialBodies() {
     // Mars
     glBegin(GL_POLYGON);
     glColor3ub(198, 62, 60);
-    for (int i = 0; i <= 360; i++) {
+    for (int i = 0; i <= 360; i++)
+    {
         theta = i * 3.142 / 180;
         glVertex2f(70 * cos(theta) + mars_x, 70 * sin(theta) + mars_y);
     }
@@ -213,7 +235,8 @@ void drawCelestialBodies() {
     // Jupiter
     glBegin(GL_POLYGON);
     glColor3ub(214, 206, 158);
-    for (int i = 0; i <= 360; i++) {
+    for (int i = 0; i <= 360; i++)
+    {
         theta = i * 3.142 / 180;
         glVertex2f(120 * cos(theta) + jupiter_x, 120 * sin(theta) + jupiter_y);
     }
@@ -224,7 +247,8 @@ void drawCelestialBodies() {
     glBegin(GL_POLYGON);
     // Ring color
     glColor3ub(231, 203, 191);
-    for (int i = 0; i <= 360; i++) {
+    for (int i = 0; i <= 360; i++)
+    {
         theta = i * 3.142 / 180;
         // Ellipse where x is longer than y
         glVertex2f(130 * cos(theta) + saturn_x, 60 * sin(theta) + saturn_y);
@@ -236,7 +260,8 @@ void drawCelestialBodies() {
 
     // Space color (between the ring and the planet)
     glColor3ub(0, 0, 0);
-    for (int i = 0; i <= 360; i++) {
+    for (int i = 0; i <= 360; i++)
+    {
         theta = i * 3.142 / 180;
         glVertex2f(110 * cos(theta) + saturn_x, 50 * sin(theta) + saturn_y);
     }
@@ -245,7 +270,8 @@ void drawCelestialBodies() {
     // Saturn (drawn on top of the rings)
     glBegin(GL_POLYGON);
     glColor3ub(227, 197, 101);
-    for (int i = 0; i <= 360; i++) {
+    for (int i = 0; i <= 360; i++)
+    {
         theta = i * 3.142 / 180;
         glVertex2f(90 * cos(theta) + saturn_x, 90 * sin(theta) + saturn_y);
     }
@@ -254,7 +280,8 @@ void drawCelestialBodies() {
     // Uranus
     glBegin(GL_POLYGON);
     glColor3ub(36, 97, 253);
-    for (int i = 0; i <= 360; i++) {
+    for (int i = 0; i <= 360; i++)
+    {
         theta = i * 3.142 / 180;
         glVertex2f(80 * cos(theta) + uranus_x, 80 * sin(theta) + uranus_y);
     }
@@ -263,14 +290,16 @@ void drawCelestialBodies() {
     // Neptune
     glBegin(GL_POLYGON);
     glColor3ub(153, 223, 254);
-    for (int i = 0; i <= 360; i++) {
+    for (int i = 0; i <= 360; i++)
+    {
         theta = i * 3.142 / 180;
         glVertex2f(80 * cos(theta) + neptune_x, 80 * sin(theta) + neptune_y);
     }
     glEnd();
 }
 
-void display() {
+void display()
+{
     // Clearing color buffer
     glClear(GL_COLOR_BUFFER_BIT);
 
@@ -283,7 +312,8 @@ void display() {
     glutSwapBuffers();
 }
 
-void init() {
+void init()
+{
     // Space color (black)
     glClearColor(0, 0, 0, 0);
 
@@ -302,15 +332,16 @@ void init() {
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
-
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
 
     // Obtaining star positions using `rand()`
     // Modulus is used because no remainder is bigger than dividend, thus `rand() % 3800` will return max of 3799
     // Subtract by 1900 to get both positive and negative coordinates, from -1900 to 1899 (ortho2D bounds)
-    for (int i = 0; i < 200; i++) {
-        zx[i] = rand() % 3800 - 1900;
-        zy[i] = rand() % 3800 - 1900;
+    for (int i = 0; i < 200; i++)
+    {
+        stars[i][0] = rand() % 3800 - 1900;
+        stars[i][1] = rand() % 3800 - 1900;
     }
 
     glutInit(&argc, argv);
@@ -337,111 +368,131 @@ int main(int argc, char **argv) {
     return 0;
 }
 
-void timer(int) {
+void timer(int)
+{
     // Call display function again
     glutPostRedisplay();
 
-    if (!stop) {
+    if (!stop)
+    {
         // Call timer again at time 10
         glutTimerFunc(10, timer, 0);
     }
 
     // Update Sun halo radius
-    sun_r += sr;
-    if (sun_r == 250) {
-        sr = 1;
+    sun_radius += sun_halo_radius;
+    if (sun_radius == 250)
+    {
+        sun_halo_radius = 1;
     }
-    if (sun_r == 300) {
-        sr = -1;
+    if (sun_radius == 300)
+    {
+        sun_halo_radius = -1;
     }
 
     // Update Neptune angular position
-    if (n < 360) {
-        theta = n * 3.142 / 180;
+    if (neptune_angle < 360)
+    {
+        theta = neptune_angle * 3.142 / 180;
         neptune_x = 1800 * cos(theta);
         neptune_y = 1800 * sin(theta);
     }
-    if (n >= 360) {
-        n = 0;
+    if (neptune_angle >= 360)
+    {
+        neptune_angle = 0;
     }
 
     // Update Uranus angular position
-    if (u < 360) {
-        theta = u * 3.142 / 180;
+    if (uranus_angle < 360)
+    {
+        theta = uranus_angle * 3.142 / 180;
         uranus_x = 1600 * cos(theta);
         uranus_y = 1600 * sin(theta);
     }
-    if (u >= 360) {
-        u = 0;
+    if (uranus_angle >= 360)
+    {
+        uranus_angle = 0;
     }
 
     // Update Saturn angular position
-    if (s < 360) {
-        theta = s * 3.142 / 180;
+    if (saturn_angle < 360)
+    {
+        theta = saturn_angle * 3.142 / 180;
         saturn_x = 1400 * cos(theta);
         saturn_y = 1400 * sin(theta);
     }
-    if (s >= 360) {
-        s = 0;
+    if (saturn_angle >= 360)
+    {
+        saturn_angle = 0;
     }
 
     // Update Jupiter angular position
-    if (j < 360) {
-        theta = j * 3.142 / 180;
+    if (jupiter_angle < 360)
+    {
+        theta = jupiter_angle * 3.142 / 180;
         jupiter_x = 1200 * cos(theta);
         jupiter_y = 1200 * sin(theta);
     }
-    if (j >= 360) {
-        j = 0;
+    if (jupiter_angle >= 360)
+    {
+        jupiter_angle = 0;
     }
 
     // Update Mars angular position
-    if (mr < 360) {
-        theta = mr * 3.142 / 180;
+    if (mars_angle < 360)
+    {
+        theta = mars_angle * 3.142 / 180;
         mars_x = 1000 * cos(theta);
         mars_y = 1000 * sin(theta);
     }
-    if (mr >= 360) {
-        mr = 0;
+    if (mars_angle >= 360)
+    {
+        mars_angle = 0;
     }
 
     // Update Earth angular position
-    if (e < 360) {
-        theta = e * 3.142 / 180;
+    if (earth_angle < 360)
+    {
+        theta = earth_angle * 3.142 / 180;
         earth_x = 800 * cos(theta);
         earth_y = 800 * sin(theta);
     }
-    if (e >= 360) {
-        e = 0;
+    if (earth_angle >= 360)
+    {
+        earth_angle = 0;
     }
 
     // Update Venus angular position
-    if (v < 360) {
-        theta = v * 3.142 / 180;
+    if (venus_angle < 360)
+    {
+        theta = venus_angle * 3.142 / 180;
         venus_x = 600 * cos(theta);
         venus_y = 600 * sin(theta);
     }
-    if (v >= 360) {
-        v = 0;
+    if (venus_angle >= 360)
+    {
+        venus_angle = 0;
     }
 
     // Update Mercury angular position
-    if (m < 360) {
-        theta = m * 3.142 / 180;
+    if (mercury_angle < 360)
+    {
+        theta = mercury_angle * 3.142 / 180;
         mercury_x = 400 * cos(theta);
         mercury_y = 400 * sin(theta);
     }
-    if (m >= 360) {
-        m = 0;
+    if (mercury_angle >= 360)
+    {
+        mercury_angle = 0;
     }
 
     // Angular position updates calculated respective to Earth
-    n += (0.006 * 2);
-    u += (0.012 * 2);
-    s += (0.034 * 2);
-    j += (0.084 * 2);
-    mr += (0.53 * 2);
-    e += (1 * 2);
-    v += (1.62 * 2);
-    m += (4.16 * 2);
+    neptune_angle += (0.006 * 2);
+    uranus_angle += (0.012 * 2);
+    saturn_angle += (0.034 * 2);
+    jupiter_angle += (0.084 * 2);
+    mars_angle += (0.53 * 2);
+    earth_angle += (1 * 2);
+    venus_angle += (1.62 * 2);
+    mercury_angle += (4.16 * 2);
 }
